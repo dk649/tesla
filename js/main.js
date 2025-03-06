968de44fdb7246f65c91a5eeba2fd267947b5ac0
dk649 <58308212+dk649@users.noreply.github.com>
2025-03-06 16:23:06 +0000

    updates

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 window.onload = function () {
-  console.log("working");
-
   const topBar = document.getElementById("top-bar");
-  console.log(topBar);
+  const exteriorColorSection = document.getElementById("exterior-buttons");
+  const interiorColorSection = document.getElementById("interior-buttons");
+  const exteriorImage = document.getElementById("exterior-image");
+  const interiorImage = document.getElementById("interior-image");
 
   const handleScroll = () => {
     const atTop = window.scrollY === 0;
@@ -10,7 +11,42 @@ window.onload = function () {
     topBar.classList.toggle("hidden-bar", !atTop);
   };
 
-  window.addEventListener("scroll", () => requestAnimationFrame(handleScroll));
+  const exteriorImages = {
+    "Stealth Grey": "/images/model-y-stealth-grey.jpg",
+    "Pearl White": "/images/model-y-pearl-white.jpg",
+    "Deep Blue": "/images/model-y-deep-blue-metallic.jpg",
+    "Solid Black": "/images/model-y-solid-black.jpg",
+    "Ultra Red": "/images/model-y-ultra-red.jpg",
+    Quicksilver: "/images/model-y-quicksilver.jpg",
+  };
 
-  // your code here
+  const interiorImages = {
+    Dark: "./images/model-y-interior-dark.jpg",
+    Light: "./images/model-y-interior-light.jpg",
+  };
+
+  const handleColorButtonClick = (event) => {
+    let button;
+    if (event.target.tagName === "IMG") {
+      button = event.target.closest("button");
+    } else if (event.target.tagName === "BUTTON") {
+      button = event.target;
+    }
+
+    if (button) {
+      const buttons = event.currentTarget.querySelectorAll("button");
+      buttons.forEach((btn) => btn.classList.remove("btn-selected"));
+      button.classList.add("btn-selected");
+      if (event.currentTarget === exteriorColorSection) {
+        const color = button.querySelector("img").alt;
+        console.log(color);
+        exteriorImage.src = exteriorImages[color];
+      }
+    }
+  };
+
+  interiorColorSection.addEventListener("click", handleColorButtonClick);
+  exteriorColorSection.addEventListener("click", handleColorButtonClick);
+
+  window.addEventListener("scroll", () => requestAnimationFrame(handleScroll));
 };
